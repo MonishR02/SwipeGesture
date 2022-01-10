b063a03fecd0f3b4c69dff5d7ec58df3488c5afb
monish <monishraja0200@gmail.com>
2022-01-10 16:23:16 +0530

    Initial Commit

---
+++ SwipeGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeGesture
+//
+//  Created by monish-pt4670 on 20/10/1943 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
